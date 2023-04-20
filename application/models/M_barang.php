84fd1fd9874477a59d40e98761e4ad509746ae07
rumi0811 <sekarpuspitaarum08@gmail.com>
2023-04-20 12:38:39 +0700

    Preview gambar

---
+++ application/models/M_barang.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_barang extends CI_Model {
+    public function get_all_data(){
+        $this->db->select('*');
+        $this->db->from('tbl_barang');
+        $this->db->join('tbl_kategori', 'tbl_kategori.id_kategori = tbl_barang.id_kategori', 'left');
+        
+        $this->db->order_by('tbl_barang.id_barang', 'desc');      
+        return $this->db->get()->result();
+        
+      
+    }
+
+    public function add($data){
+        $this->db->insert('tbl_kategori', $data); 
+    }
+
+    public function edit($data){
+        $this->db->where('id_barang', $data['id_barang']);
+        $this->db->update('tbl_barang', $data);
+        
+    }
+    
+    public function delete($data){
+        $this->db->where('id_barang', $data['id_barang']);
+        $this->db->delete('tbl_barang', $data);
+        
+    }
+}
---
+++ application/views/barang/v_add.php
@@ -0,0 +1,92 @@
+<div class="col-md-12">
+            <!-- general form elements disabled -->
+        <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Input Barang</h3>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body">
+                <?php 
+                echo form_open_multipart('barang/add') ?>
+
+                <div class="form-group">
+                        <label>Nama Barang</label>
+                        <input name="nama_barang"  class="form-control" placeholder="Nama Barang" value="<?= set_value('nama_barang') ?>">
+                      </div>
+                    
+
+
+                <div class="row">
+                      <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Kategori</label>
+                        <select name="id_kategori" class="form-control">
+                        <option value="">---Pilih Kategori---</option>
+                            <?php foreach ($kategori as $key => $value) { ?>
+                                <option value="<?= $value->id_kategori ?>"><?= $value->nama_kategori ?></option>
+                         <?php } ?>
+                        </select>
+                      </div>
+                    </div>
+
+
+                    <div class="col-sm-6">
+                      <div class="form-group">
+                        <label>Harga Barang</label>
+                        <input name="harga"  class="form-control" placeholder="Harga Barang" value="<?= set_value('harga') ?>">
+                      </div>                     
+                    </div>
+                  </div>
+
+                    <div class="form-group">
+                        <label>Deskripsi Barang</label>
+                        <textarea name="deskripsi" class="form-control" rows="5" placeholder="Deskripsi Barang"><?= set_value('deskripsi') ?></textarea>
+                    </div>
+
+<!-- memilih image untuk dimasukkan -->
+                    <div class="row">
+                    <div class="col-sm-10">
+                    <div class="form-group">
+                        <label>Foto Barang</label>
+                        <input type="file" name="gambar" class="form-control" id="preview_gambar" required>
+                    </div>
+                    </div>
+<!-- memunculkan image di samping file choosen -->
+                    <div class="col-sm-2">
+                    <div class="form-group">
+                        <img src="<?=  base_url('assets/gambar/no-photo.png') ?>" id="gambar_load" width="200px">
+                    </div>
+                    </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+                        <a href="<?= base_url('barang') ?>" class="btn btn-success btn-sm">Kembali</a>
+                    </div>
+
+                
+
+                <?php 
+                echo form_close() ?>
+              </div>
+        
+</div>
+</div>
+
+<script>
+    function bacaGambar(input){
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#gambar_load').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $("#preview_gambar").change(function(){
+        bacaGambar(this);
+
+    });
+</script>
